79eb2567d9d39152e88c906a7722379e4c994c7a
Zé Bateira <jlageb+github@gmail.com>
2013-10-04 11:34:48 +0100

    Hover animation over navigation item added.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
 
+$(document).ready(function(){
+	var nav = {
+		init: function(config) {
+		},
+
+		events: {
+			navItemHover: function() {
+				$('.nav-item').mouseover(function() {
+					$(this).slideUp();
+				});
+			},
+
+			loadEvents: function() {
+				this.navItemHover();
+			}
+		}
+	}
+
+	// nav.events.loadEvents();
+
+});
