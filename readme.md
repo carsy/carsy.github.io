daf2191b83b9f2ce7500467ad0233b8690bf122a
carsy <jlageb@gmail.com>
2017-10-26 20:15:57 +0100

    v2.2: animations

--- readme.md
+++
@@ -1,3 +0,0 @@
-# My personal website
-
-It's personal. And it's a website. So it's my personal website.
--- src/animations/styles.js
+++
@@ -1,3 +0,0 @@
-
-export const animation = ({ animation, duration = 1000, func = 'ease', delay = 0 } = {}) =>
-    `${animation} ${duration}ms ${func} ${delay}ms forwards`;
